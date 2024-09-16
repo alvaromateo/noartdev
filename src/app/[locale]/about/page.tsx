5358ec3a5c43eea829af260a14c8a8f9b03ab65e
Alvaro Mateo <alvaromateo9@gmail.com>
2024-09-17 00:04:29 +0200

    About page mobile styles

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -10,7 +10,7 @@ import Card from '@/src/components/about/card'
 import SocialIcons from '@/src/components/utils/social-icons'
 
 import portrait from '/assets/photo-linkedin.jpg'
-import { WithStartAndEnd } from '@/src/global/types'
+
 
 const imageDiameter = 300
 const skillsLogoSize = 32
@@ -28,7 +28,7 @@ export default async function About({
   // TODO: add mobile and 'lg' (1024px) breakpoint
 
   return (
-    <main className='lg:max-w-[1024px] mx-auto pt-8 pb-16 content'>
+    <main className='px-[8%] lg:max-w-[1024px] lg:px-12 lg:mx-auto pt-8 pb-16 content'>
       <div className={`
         grid gap-y-12 grid-cols-1 grid-rows-[auto_auto_auto]
         md:gap-x-16 md:gap-y-16 md:grid-cols-2 md:grid-rows-[auto_auto]
@@ -59,14 +59,14 @@ export default async function About({
           </figure>
         </div>
         <div className={`
-          pl-[10%] md:pl-0 md:w-full
-          col-start-1 row-start-1
+          md:pl-0 md:w-full
+          col-start-1 row-start-2
           md:col-start-2 md:row-start-1
           md:justify-self-start md:self-start
         `}>
-          <Card title={t('contact')} className=''>
-            <div className='flex flex-row justify-between items-center'>
-              <div>
+          <Card title={t('contact')} className='md:mt-8'>
+            <div className='flex flex-row flex-wrap justify-between items-center'>
+              <div className='mr-8'>
                 <p>alvaromateo9@gmail.com</p>
                 <p>+41 79 846 08 61</p>
               </div>
@@ -77,9 +77,11 @@ export default async function About({
           </Card>
           <Card title={t('skills.title')}
             className='bg-basic pl-16 border-l-4 border-solid box-content border-overlay-0'>
-            { createIconsRow(Object.entries(skills.langs)) }
-            { createIconsRow(Object.entries(skills.techs)) }
-            { createIconsRow(Object.entries(skills.tools)) }
+              <div className='flex flex-row flex-wrap items-center'>
+                { createIconsRow(Object.entries(skills.langs)) }
+                { createIconsRow(Object.entries(skills.techs)) }
+                { createIconsRow(Object.entries(skills.tools)) }
+              </div>
           </Card>
           <Card title={t('langs.title')} className=''>
             <div className='flex flex-col'>
@@ -88,14 +90,14 @@ export default async function About({
           </Card>
         </div>
         <div className={`
-          pl-[10%] md:pl-0
-          col-start-1 row-start-2
+          md:pl-0
+          col-start-1 row-start-3
           md:col-span-2 md:row-start-2
           md:self-start
         `}>
           <Card title={t('experience.title')} className='bg-basic mt-0 p-0'>
-            <div className='border-l-2 border-solid border-overlay-0'>
-              <div className='mt-8'>
+            <div className='md:border-l-2 md:border-solid md:border-overlay-0'>
+              <div className='md:mt-8'>
                 {
                   Object.entries(messages.About.experience)
                     .sort(compareDataByKey)
@@ -111,9 +113,9 @@ export default async function About({
               </div>
             </div>
           </Card>
-          <Card title={t('education.title')} className='bg-basic mt-16 p-0'>
-            <div className='border-l-2 border-solid border-overlay-0'>
-              <div className='mt-8'>
+          <Card title={t('education.title')} className='bg-basic mt-8 md:mt-16 p-0'>
+            <div className='md:border-l-2 md:border-solid md:border-overlay-0'>
+              <div className='md:mt-8'>
                 {
                   Object.entries(messages.About.education)
                     .sort(compareDataByKey)
@@ -146,23 +148,20 @@ function compareDataByKey([keyA]: [string, any], [keyB]: [string, any]) {
 
 function createIconsRow(icons: [string, StaticImport][]) {
   return (
-    <div className='flex flex-row flex-wrap items-center mb-4'>
-      {
-        icons.map(([key, value]) => {
-          return <Image
-            key={key}
-            src={value}
-            width={skillsLogoSize}
-            height={skillsLogoSize}
-            alt={key}
-            style={{
-              borderRadius: '0.5rem',
-              marginRight: '0.5rem'
-            }}
-          />
-        })
-      }
-    </div>
+    icons.map(([key, value]) => {
+      return <Image
+        key={key}
+        src={value}
+        width={skillsLogoSize}
+        height={skillsLogoSize}
+        alt={key}
+        style={{
+          borderRadius: '0.5rem',
+          marginRight: '0.5rem',
+          marginBottom: '0.5rem'
+        }}
+      />
+    })
   )
 }
 
@@ -181,7 +180,7 @@ function createLanguageEntries(t: (key: any) => string) {
               marginRight: '0.75rem'
             }}
           />
-          <p className='text-sm'>
+          <p className='text-xs md:text-sm'>
             {t(`langs.${key}`)} - {t(`langs.${languageProficiencyLevels[key]}`)}
           </p>
         </div>
--- src/components/about/experience.tsx
+++ src/components/about/experience.tsx
@@ -7,8 +7,8 @@ export default function Experience({
 }) {
   let title: JSX.Element = <></>
   if (isExperience(data)) {
-    title = <div className='flex justify-between'>
-      <h3 className='text-subtitle text-lg'>{data.title}</h3>
+    title = <div className='flex justify-between items-center'>
+      <h3 className='text-subtitle text-lg mr-8'>{data.title}</h3>
       <p className='text-comment'>{data.companyName}</p>
     </div>
   } else {
@@ -17,19 +17,20 @@ export default function Experience({
 
   return (
     <div className={`
-      grid grid-cols-4 md:justify-items-stretch md:items-center
-      py-6
+      grid grid-cols-1 gird-rows-2 md:grid-cols-4 md:grid-rows-1
+      justify-items-stretch items-center
+      py-6 text-xs md:text-base
     `}>
       <div className={`
-        md:col-start-1 flex flex-col items-center
-        text-comment text-sm
+        col-start-1 flex flex-row md:flex-col items-center
+        justify-between text-comment text-sm
       `}>
-        <p>{data.dateStart}</p>
-        <p> - </p>
+        <p className='mr-8 md:mr-0'>{data.dateStart}</p>
+        <p className='hidden md:block'> - </p>
         <p>{data.dateEnd}</p>
       </div>
       <div className={`
-        md:col-span-3
+        md:col-span-3 mt-4 md:mt-0
       `}>
         {title}
         <ul className='mt-4'>
